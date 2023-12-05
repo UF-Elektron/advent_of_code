68b163a68fb852046e2a518b83a35185b3203648
UF-Elektron <dave.mueller@gmx.net>
2023-12-05 14:27:03 +0100

    fixed issue and completed advent 3 (missing file)

--- advent_3.py
+++ advent_3.py
@@ -6,26 +6,27 @@ part_numbers = []
 invalid_numbers = []
 data_buffer = []
 
-with open("test_input3", "r") as f:
+with open("my_input3", "r") as f:
     while (x:= f.readline()):
-        data_buffer.append(x)
+        data_buffer.append(x.rstrip() + '.')
 
 for i in data_buffer:
     print(i)
 
-line_before = data_buffer[0]
-line_current = data_buffer[1]
-line_after = data_buffer[2]
+line_before = '.'
+line_current = data_buffer[0]
+line_after = data_buffer[1]
 
 def symbol_nearby(pos):
     for line in [line_before, line_after, line_current]:
         for check_el_offset in checked_element_offsets:
             try:
                 if line[pos + check_el_offset] not in not_a_symbol:
-                    print(f"symbol found: {line[pos + check_el_offset]}")
+                    print(f"symbol found: {line[pos + check_el_offset]}\n in {line=}\n at {pos=}")
                     return True
             except:
-                print("out of matrix")
+                None
+                #print("out of matrix")
     return False
 
 for i, x in enumerate(data_buffer):
@@ -48,18 +49,18 @@ for i, x in enumerate(data_buffer):
     try:
         line_current = data_buffer[i + 1]
     except:
-        print("end of data buffer")
-        line_after = ['.']
+        print("completed")
+        break
     try:
         line_after = data_buffer[i + 2]
     except:
         print("end of data buffer")
-        line_after = ['.']
+        line_after = '.'
 
 
 total = 0
 for part_nr in part_numbers:
     total += int(part_nr)
 print(part_numbers)
-print(invalid_numbers)
+#print(invalid_numbers)
 print(f"part number total: " + str(total))
