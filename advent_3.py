0eb5d42e73d144d7caf33e89b64da882915a4269
UF-Elektron <dave.mueller@gmx.net>
2023-12-05 15:53:56 +0100

    this turned out messy... adv3.2 completed

--- advent_3.py
+++ advent_3.py
@@ -1,5 +1,6 @@
 digits = '0123456789'
 not_a_symbol = digits + '.'
+a_gear = '*'
 checked_element_offsets = [-1, 0, 1]
 
 part_numbers = []
@@ -8,10 +9,7 @@ data_buffer = []
 
 with open("my_input3", "r") as f:
     while (x:= f.readline()):
-        data_buffer.append(x.rstrip() + '.')
-
-for i in data_buffer:
-    print(i)
+        data_buffer.append(x.rstrip() + '..')
 
 line_before = '.'
 line_current = data_buffer[0]
@@ -26,41 +24,74 @@ def symbol_nearby(pos):
                     return True
             except:
                 None
-                #print("out of matrix")
     return False
+
+gears_with_numbers = {}
+def gear_nearby(pos, line_nr):
+    res = []
+    for line_offset in checked_element_offsets:
+        line_nr_tmp = line_nr + line_offset
+        try:
+            line = data_buffer[line_nr_tmp]
+        except:
+            line = '.'
+        for check_el_offset in checked_element_offsets:
+            try:
+                gear_pos = pos + check_el_offset
+                if line[pos + check_el_offset] in a_gear:
+                    print(f"gear '*' found: {line_nr_tmp=} {gear_pos=}")
+                    res.append((line_nr_tmp, gear_pos))
+            except:
+                None
+    return res
 
 for i, x in enumerate(data_buffer):
     tmp_nr = ''
     valid = False
-    for pos, el in enumerate(x): # + '.'?
+    line_coord = []
+    for pos, el in enumerate(x):
         if el in digits:
             # it's a number
             tmp_nr += el
             if symbol_nearby(pos):
                 valid = True
+            if line_coord == []:
+                line_coord = gear_nearby(pos, i)
+                print(f"received {line_coord=}")
         else:
-            if valid:
-                part_numbers.append(tmp_nr)
-            else:
-                invalid_numbers.append(tmp_nr)
+            if tmp_nr != '':
+                if valid:
+                    part_numbers.append(tmp_nr)
+                else:
+                    invalid_numbers.append(tmp_nr)
+                for lc in line_coord:
+                    try:
+                        gears_with_numbers.get(lc[0]).get(lc[1]).append(tmp_nr)
+                        print(f"updated entry for {lc=}")
+                    except:
+                        try:
+                            print(f"updated ***************** entry for {lc=}")
+                            gears_with_numbers.get(lc[0]).update({lc[1]: [tmp_nr]})
+                        except:
+                            print(f"create new entry for {lc=}")
+                            gears_with_numbers.update({lc[0]: {lc[1]: [tmp_nr]}})
+            line_coord = []
             tmp_nr = ''
             valid = False
-    line_before = data_buffer[i]
-    try:
-        line_current = data_buffer[i + 1]
-    except:
-        print("completed")
-        break
-    try:
-        line_after = data_buffer[i + 2]
-    except:
-        print("end of data buffer")
-        line_after = '.'
-
 
 total = 0
 for part_nr in part_numbers:
     total += int(part_nr)
 print(part_numbers)
-#print(invalid_numbers)
 print(f"part number total: " + str(total))
+
+print(gears_with_numbers)
+summed_up = 0
+for k in gears_with_numbers.keys():
+    for k2 in gears_with_numbers[k].keys():
+        if len(gears_with_numbers[k][k2]) == 2:
+            print(gears_with_numbers[k][k2])
+            summed_up += int(gears_with_numbers[k][k2][0]) * int(gears_with_numbers[k][k2][1])
+        else:
+            print(f"invalid length: {gears_with_numbers[k][k2]}")
+print(summed_up)
