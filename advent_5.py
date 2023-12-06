617807c71e558fba8f4d25ea6653f460788c4e3b
UF-Elektron <dave.mueller@gmx.net>
2023-12-06 23:30:13 +0100

    completed advent 5 part 1

--- advent_5.py
+++ advent_5.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python3
-TESTING = True
-
-# concept stuff...
-source_start = 0
-destination_start = 0
-map_delta = destination_start - source_start
-map_range = 0
-
-# for source in range(0, 100):
-#     if source >= source_start and source < source_start + map_range:
-        # print(f"{source=} = {(source+map_delta)=}")
+TESTING = False
 
 if TESTING:
     file_name = 'test_input5'
@@ -17,6 +7,7 @@ else:
     file_name = 'my_input5'
 
 maps = {}
+map_names = []
 
 # read file and get data
 with open(file_name, 'r') as f:
@@ -25,9 +16,11 @@ with open(file_name, 'r') as f:
         line = x.rstrip()
         if "seeds: " in line:
             seeds = line.split("seeds: ")[1].split(" ")
+            seeds = [int(s) for s in seeds]
         elif " map:" in line:
             map_name = line.split(" map:")[0]
-            print(map_name)
+            map_names.append(map_name)
+            #print(map_name)
         else:
             try:
                 destination_start, source_start, map_range = line.split(' ')
@@ -35,16 +28,32 @@ with open(file_name, 'r') as f:
                 source_start = int(source_start)
                 map_range = int(map_range)
             except:
-                print(f"... empty line: '{line}'")
+                # empty line
+                #print(f"... empty line: '{line}'")
                 continue
 
             if destination_start != '':
-                map_info = {"source_start": source_start, "destination_start": destination_start, 'map_range': map_range}
+                map_info = {'source_start': source_start, 'destination_start': destination_start, 'map_range': map_range}
                 try:
                     maps[map_name].append(map_info)
                 except:
                     maps.update({map_name: [map_info]})
 
-for k in maps.keys():
-    print(k)
-    print(maps[k])
+# map values
+all_results = []
+for seed in seeds:
+    mapped_value = seed
+    for name in map_names:
+        for m in maps[name]:
+            source_start = m['source_start']
+            destination_start = m['destination_start']
+            map_range = m['map_range']
+            map_delta = destination_start - source_start
+            #print(f"{source_start} <= my_val < {source_start + map_range}")
+            if mapped_value >= source_start and mapped_value < source_start + map_range:
+                mapped_value = mapped_value + map_delta
+                break
+    all_results.append(mapped_value)
+    print(f"{seed}: {mapped_value}")
+
+print(min(all_results))
