cc91671fc546b321816eb3d50e94686cde32dfc5
UF-Elektron <dave.mueller@gmx.net>
2023-12-07 22:03:00 +0100

    adv 5 solution does not scale for part 2!

--- advent_5.py
+++ advent_5.py
@@ -15,8 +15,8 @@ with open(file_name, 'r') as f:
     while x:=f.readline():
         line = x.rstrip()
         if "seeds: " in line:
-            seeds = line.split("seeds: ")[1].split(" ")
-            seeds = [int(s) for s in seeds]
+            seeds_data = line.split("seeds: ")[1].split(" ")
+            seeds_data = [int(s) for s in seeds_data]
         elif " map:" in line:
             map_name = line.split(" map:")[0]
             map_names.append(map_name)
@@ -39,21 +39,43 @@ with open(file_name, 'r') as f:
                 except:
                     maps.update({map_name: [map_info]})
 
-# map values
+# wow... my solution from part 1 does not scale at all!
+min_value = None
 all_results = []
-for seed in seeds:
-    mapped_value = seed
-    for name in map_names:
-        for m in maps[name]:
-            source_start = m['source_start']
-            destination_start = m['destination_start']
-            map_range = m['map_range']
-            map_delta = destination_start - source_start
-            #print(f"{source_start} <= my_val < {source_start + map_range}")
-            if mapped_value >= source_start and mapped_value < source_start + map_range:
-                mapped_value = mapped_value + map_delta
-                break
-    all_results.append(mapped_value)
-    print(f"{seed}: {mapped_value}")
+# part 2 with seed ranges
+seeds_length = len(seeds_data)
+print(f"{seeds_length=}")
+seeds_generator = []
+for i in range(0, seeds_length, 2):
+    print(f"create iterator {i}")
+    seeds_generator = (seeds_data[0 + i] + s for s in range(seeds_data[1 + i]))
+    #seeds_generator.append(x)
+    #break
 
-print(min(all_results))
+    #print(seeds)
+    # map values
+    #for seeds in seeds_generator:
+    for seed in seeds_generator:
+        mapped_value = seed
+        for name in map_names:
+            for m in maps[name]:
+                source_start = m['source_start']
+                destination_start = m['destination_start']
+                map_range = m['map_range']
+                map_delta = destination_start - source_start
+                #print(f"{source_start} <= my_val < {source_start + map_range}")
+                if mapped_value >= source_start and mapped_value < source_start + map_range:
+                    mapped_value = mapped_value + map_delta
+                    break
+        if min_value:
+            if min_value > mapped_value:
+                min_value = mapped_value
+        else:
+            min_value = mapped_value
+        # all_results.append(mapped_value)
+        #print(f"{seed}: {mapped_value}")
+    # part 1
+    #print(min(all_results))
+    # part 2
+    print(min_value)
+    # 46 +
