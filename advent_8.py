d109dd68104fcbfeb1ba23ab34e9f94ed96a9bcb
UF-Elektron <dave.mueller@gmx.net>
2023-12-08 18:55:34 +0100

    completed adv8 part 1

--- advent_8.py
+++ advent_8.py
@@ -20,3 +20,18 @@ with open(file_name, 'r') as f:
 
 print(directions)
 print(coordinates)
+
+# find 'ZZZ'
+location = 'AAA'
+iterations = 0
+total_steps = 0
+while location != 'ZZZ':
+    total_steps += 1
+    next_diretion = directions[iterations]
+    location = coordinates[location][next_diretion]
+    print(f"new location = {location}")
+    if iterations == len(directions) - 1:
+        iterations = 0
+    else:
+        iterations += 1
+print(f"total steps required to reach destination: {total_steps}")
