f536c07bcb6ce7fd2bc1742d97be137057fb1be9
UF-Elektron <dave.mueller@gmx.net>
2023-12-08 13:06:01 +0100

    renamed early scripts to match new naming

---
+++ advent_1.1.py
@@ -0,0 +1,20 @@
+file_name = "my_input"
+results = []
+with open(file_name, "r") as f:
+    while (x:= f.readline()):
+        digits = {'first': None, 'last': None}
+        for i in x:
+            try:
+                val = int(i)
+                digits['last'] = val
+                if digits['first'] == None:
+                    digits['first'] = val
+            except:
+                None # not a digit
+        results.append(digits)
+    print(results)
+
+total = 0
+for i in results:
+    total += i['first'] * 10 + i['last']
+print(total)
