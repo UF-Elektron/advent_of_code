da2a7a2a01b496511fe6556c69f9e5cc6207d252
UF-Elektron <dave.mueller@gmx.net>
2023-12-10 15:29:34 +0100

    find least common multiple with math.lcm: completed adv8

--- advent_8.2.py
+++ advent_8.2.py
@@ -1,5 +1,5 @@
-#file_name = "input/adv8"
-file_name = "input/test_adv8"
+file_name = "input/adv8"
+#file_name = "input/test_adv8"
 
 directions = None
 # the first key is the location and the sub keys are left and right destination
@@ -18,9 +18,6 @@ with open(file_name, 'r') as f:
             l = l.strip()
             r = r.strip()
             coordinates.update({location.strip(): {'L': l, 'R':r}})
-
-#print(directions)
-#print(coordinates)
 
 # find start location: last element is 'A'
 start_indicator = 'A'
@@ -42,14 +39,17 @@ all_paths = []
 for start_lociation in start_locations:
     all_paths.append({'current_location': {start_lociation: coordinates[start_lociation]}})
 
+# prepare dictionaries in all_paths with necessary counters
 for path in all_paths:
     for destination_location in destination_locations:
         path.update({destination_location: {'reached_count': 0, 'steps': []}})
-print(all_paths)
 
 # find out how many steps are needed to reach the locations
+# ugly solution:
+# - better not loop in range but loop as long as steps to destination are not known (and repeated steps for that matter)
+# - dictionary structure a bit unwieldy... have to get key name buy doing ugly botch with list(x.keys())[0]... this means dict structure is not correct!
 direction_iterator = 0
-for i in range(10):
+for i in range(100000):
     next_diretion = directions[direction_iterator]
     for path in all_paths:
         lid = list(path['current_location'].keys())[0]
@@ -66,5 +66,32 @@ for i in range(10):
         direction_iterator = 0
     else:
         direction_iterator += 1
-print(all_paths)
-#print(all_paths)
+
+steps_delta = []
+for path in all_paths:
+    for destination_location in destination_locations:
+        x = path.get(destination_location)
+        if x['reached_count'] > 0:
+            steps_delta.append(x['steps'][0])
+            continue
+print(steps_delta)
+
+# find least common multiple (Kleinstes gemeinsames Vielfaches)
+# python math library is awesome :-)
+import math
+print(math.lcm(*steps_delta))
+
+
+# results after completing search for steps "for i in range(100000):"
+{'current_location': {'XRG': {'L': 'LNK', 'R': 'KQL'}}, 'LFZ': {'reached_count': 0, 'steps': []},
+   'HMZ': {'reached_count': 4, 'steps': [20777, 41554, 62331, 83108]},}                               # 'ZZZ': {'reached_count': 0, 'steps': []}, 'DDZ': {'reached_count': 0, 'steps': []}, 'XKZ': {'reached_count': 0, 'steps': []}, 'RNZ': {'reached_count': 0, 'steps': []}}
+{'current_location': {'LHL': {'L': 'HSC', 'R': 'LBV'}}, 'LFZ': {'reached_count': 0, 'steps': []}, 'HMZ': {'reached_count': 0, 'steps': []},
+   'ZZZ': {'reached_count': 5, 'steps': [18673, 37346, 56019, 74692, 93365]},}                        # 'DDZ': {'reached_count': 0, 'steps': []}, 'XKZ': {'reached_count': 0, 'steps': []}, 'RNZ': {'reached_count': 0, 'steps': []}}
+{'current_location': {'MBH': {'L': 'KBF', 'R': 'TSX'}}, 'LFZ': {'reached_count': 0, 'steps': []}, 'HMZ': {'reached_count': 0, 'steps': []}, 'ZZZ': {'reached_count': 0, 'steps': []}, 'DDZ': {'reached_count': 0, 'steps': []}, 'XKZ': {'reached_count': 0, 'steps': []},
+   'RNZ': {'reached_count': 7, 'steps': [13939, 27878, 41817, 55756, 69695, 83634, 97573]}}
+{'current_location': {'JNK': {'L': 'RXN', 'R': 'BQX'}}, 'LFZ': {'reached_count': 0, 'steps': []}, 'HMZ': {'reached_count': 0, 'steps': []}, 'ZZZ': {'reached_count': 0, 'steps': []}, 'DDZ': {'reached_count': 0, 'steps': []},
+   'XKZ': {'reached_count': 5, 'steps': [17621, 35242, 52863, 70484, 88105]},}                        # 'RNZ': {'reached_count': 0, 'steps': []}}
+{'current_location': {'QNX': {'L': 'SHD', 'R': 'LXG'}},
+   'LFZ': {'reached_count': 5, 'steps': [19199, 38398, 57597, 76796, 95995]},}                        # 'HMZ': {'reached_count': 0, 'steps': []}, 'ZZZ': {'reached_count': 0, 'steps': []}, 'DDZ': {'reached_count': 0, 'steps': []}, 'XKZ': {'reached_count': 0, 'steps': []}, 'RNZ': {'reached_count': 0, 'steps': []}}
+{'current_location': {'NQL': {'L': 'MGL', 'R': 'SCP'}}, 'LFZ': {'reached_count': 0, 'steps': []}, 'HMZ': {'reached_count': 0, 'steps': []}, 'ZZZ': {'reached_count': 0, 'steps': []},
+   'DDZ': {'reached_count': 8, 'steps': [12361, 24722, 37083, 49444, 61805, 74166, 86527, 98888]},}   #'XKZ': {'reached_count': 0, 'steps': []}, 'RNZ': {'reached_count': 0, 'steps': []}}
