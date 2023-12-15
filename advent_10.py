2bd286ae9a2da7c3178c48fdf01f0c4f2e9d9714
UF-Elektron <dave.mueller@gmx.net>
2023-12-15 11:00:06 +0100

    update valid directions for start position

--- advent_10.py
+++ advent_10.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-#file_name = "input/adv10"
-file_name = "input/test_adv10"
+file_name = "input/adv10"
+#file_name = "input/test_adv10"
 
 pipe_map = []
 tile_map = {'|': {'N': True, 'E': False, 'S': True, 'W': False},
@@ -11,7 +11,7 @@ tile_map = {'|': {'N': True, 'E': False, 'S': True, 'W': False},
             '7': {'N': False, 'E': False, 'S': True, 'W': True},
             'F': {'N': False, 'E': True, 'S': True, 'W': False},
             '.': {'N': False, 'E': False, 'S': False, 'W': False},
-            'S': {'N': True, 'E': True, 'S': True, 'W': True}}
+            'S': {'N': False, 'E': False, 'S': False, 'W': False}}
 direction_from = {'N': 'S', 'E': 'W', 'S': 'N', 'W': 'E'}
 dir_cycle = 'NESW'
 
@@ -51,26 +51,44 @@ with open(file_name, 'r') as f:
         pipe_map.append(x)
 
 start_pos = get_start()
+
+print(tile_map['S'])
+
+# update starting connections on tile_map
+for dir in dir_cycle:
+    tmp_pos = move_pos(dir, **start_pos)
+    tile = get_tile(**tmp_pos)
+    if tile_map[tile][direction_from[dir]]:
+        tile_map['S'].update({dir: True})
+
+print(tile_map['S'])
+
 print(f"Start at {start_pos}")
 current_position = start_pos
 new_position = start_pos
 steps = 0
 current_direction = 'N'
-for i in range(100):
+# TODO: check only valid directions of current position!
+for i in range(5000):
     current_direction = get_direction(current_direction)
     new_position = move_pos(current_direction, **current_position)
-    new_tile = get_tile(**new_position)
+    try:
+        new_tile = get_tile(**new_position)
+    except:
+        # don't fall of the edge!
+        continue
     if tile_map[new_tile][direction_from[current_direction]]:
+        print(f"current pos: {current_position}")
+        print(f"new pos: {new_position}")
         current_position = new_position
         current_direction = direction_from[current_direction]
         print(get_tile(**current_position))
         steps += 1
+        if current_position == start_pos:
+            print(f"completed circle in {steps} steps")
+            break
     else:
         None
         #print("no path")
-
-    if current_position == start_pos:
-        print(f"completed circle in {steps} steps")
-        break
 
 print(f"farthest position: {steps /2 }")
