9f2be40c73c82b333ed796b52202f84776f863a7
UF-Elektron <dave.mueller@gmx.net>
2023-12-15 10:42:58 +0100

    complete round trip and count steps

--- advent_10.py
+++ advent_10.py
@@ -12,8 +12,16 @@ tile_map = {'|': {'N': True, 'E': False, 'S': True, 'W': False},
             'F': {'N': False, 'E': True, 'S': True, 'W': False},
             '.': {'N': False, 'E': False, 'S': False, 'W': False},
             'S': {'N': True, 'E': True, 'S': True, 'W': True}}
-dir_map = {'N': 'S', 'E': 'W', 'S': 'N', 'W': 'E'}
-dir_cycle = ['NESW']
+direction_from = {'N': 'S', 'E': 'W', 'S': 'N', 'W': 'E'}
+dir_cycle = 'NESW'
+
+def get_direction(dir):
+    pos = dir_cycle.find(dir)
+    try:
+        new_dir = dir_cycle[pos + 1]
+    except:
+        new_dir = dir_cycle[0]
+    return new_dir
 
 def move_pos(direction, x, y):
     if direction == 'N':
@@ -26,25 +34,43 @@ def move_pos(direction, x, y):
         x -= 1
     else:
         raise Exception(f"ERROR: invalid direction '{direction}' passed to function move_pos")
-    return (x, y)
+    return {'x': x, 'y': y}
 
-def get_direction_from(direction_to):
-    return dir_map(direction_to)
+def get_tile(x, y):
+    return pipe_map[y][x]
 
 def get_start():
     for y, row in enumerate(pipe_map):
         for x, tile in enumerate(row):
             if tile == 'S':
-                return (x, y)
+                return {'x': x, 'y': y}
     raise Exception(f"ERROR: start position not found")
 
 with open(file_name, 'r') as f:
     while x:= f.readline().strip():
         pipe_map.append(x)
 
+start_pos = get_start()
+print(f"Start at {start_pos}")
+current_position = start_pos
+new_position = start_pos
+steps = 0
+current_direction = 'N'
+for i in range(100):
+    current_direction = get_direction(current_direction)
+    new_position = move_pos(current_direction, **current_position)
+    new_tile = get_tile(**new_position)
+    if tile_map[new_tile][direction_from[current_direction]]:
+        current_position = new_position
+        current_direction = direction_from[current_direction]
+        print(get_tile(**current_position))
+        steps += 1
+    else:
+        None
+        #print("no path")
 
-x, y = get_start()
-print(f"Start at {x}x {y}y")
-start_pos = {'x': x, 'y': y}
-print(start_pos)
-#print(move_pos('S', x, y))
+    if current_position == start_pos:
+        print(f"completed circle in {steps} steps")
+        break
+
+print(f"farthest position: {steps /2 }")
